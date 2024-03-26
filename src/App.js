28309b765ac2921e19aae3e1a5b85a95d175f0da
anushreej537 <anushreej537@gmail.com>
2024-03-26 18:04:53 +0530

    bylink

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
+import Services from './component/Services';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import {Route,Routes,Link} from 'react-router-dom'
+// import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=> {
+    return(
+      <>
+      <Router>
+        <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/services' element={<Services/>}></Route>
+        </Routes>
+      </Router>
+
+      {/* <Link to='/' className='nav-link active'>Home</Link>
+      <Link to='/about' className='nav-link'>About</Link>
+      <Link to='/contact' className='nav-link'>Contact</Link>
+      <Link to='/services' className='nav-link'>Services</Link> */}
+      </>
+    )
 }
 
 export default App;
---
+++ src/component/Services.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Services=()=>{
+    return (
+        <>
+        <h1>Services is runnnn</h1>
+        </>
+    )
+}
+export default Services;
